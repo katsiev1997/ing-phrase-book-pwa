8dbd2c2f46db0dde8a727c06cf0c03414c34a216
Mikail Katsiev <105316523+katsiev1997@users.noreply.github.com>
2024-12-02 09:54:53 +0300

    hide add-phrase

--- src/app/add-phrase/page.tsx
+++ src/app/add-phrase/page.tsx
@@ -1,12 +1,8 @@
-import { PhraseForm } from "@/src/features/phrase-form";
+// import { PhraseForm } from "@/src/features/phrase-form";
 import React from "react";
 
 const AddPhrasePage = () => {
-    return (
-        <div className="max-w-md mx-auto p-4 shadow-md rounded-lg">
-            <PhraseForm />
-        </div>
-    );
+    return <div className="max-w-md mx-auto p-4 shadow-md rounded-lg">{/* <PhraseForm /> */}</div>;
 };
 
 export default AddPhrasePage;
